77281e90ac090ed52b8f4885b0be1583c3284375
Marc Volker Dickmann <hq@devdiary.org>
2015-08-20 16:58:39 +0200

    Finished the integration of the bash color codes!

--- dutils.h
+++ dutils.h
@@ -8,22 +8,40 @@
 /* BASH COLORS */
 
 #define BC_TXT_BLACK "\e[0;30m"		/* Black */
-#define BC_TXT_GREY "\e[1;30m"		/* Grey */
 #define BC_TXT_RED "\e[0;31m"		/* Red */
 #define BC_TXT_GREEN "\e[0;32m"		/* Green */
-#define BC_TXT_BROWN "\e[0;33m"		/* Brown */
-#define BC_TXT_YELLOW "\e[1;33m"	/* Yellow */
+#define BC_TXT_YELLOW "\e[0;33m"	/* Yellow */
 #define BC_TXT_BLUE "\e[0;34m"		/* Blue */
 #define BC_TXT_PURPLE "\e[0;35m"	/* Purple */
 #define BC_TXT_CYAN "\e[0;36m"		/* Cyan */
-#define BC_TXT_WHT "\e[1;37m"		/* White */
+#define BC_TXT_WHITE "\e[0;37m"		/* White */
 
-#define BC_TXT_LGREY "\e[0;37m"		/* Light Grey */
-#define BC_TXT_LRED "\e[1;31m"		/* Light Red */
-#define BC_TXT_LGREEN "\e[1;32m"	/* Light Green */
-#define BC_TXT_LBLUE "\e[1;34m"		/* Light Blue */
-#define BC_TXT_LPURPLE "\e[1;35m"	/* Light Purple */
-#define BC_TXT_LCYAN "\e[1;36m"		/* Light Cyan */
+#define BC_BLD_BLACK "\e[1;30m"		/* Black - Bold */
+#define BC_BLD_RED "\e[1;31m"		/* Red */
+#define BC_BLD_GREEN "\e[1;32m"		/* Green */
+#define BC_BLD_YELLOW "\e[1;33m"	/* Yellow */
+#define BC_BLD_BLUE "\e[1;34m"		/* Blue */
+#define BC_BLD_PURPLE "\e[1;35m"	/* Purple */
+#define BC_BLD_CYAN "\e[1;36m"		/* Cyan */
+#define BC_BLD_WHITE "\e[1;37m"		/* White */
+
+#define BC_UND_BLACK "\e[4;30m"         /* Black - Underline*/
+#define BC_UND_RED "\e[4;31m"           /* Red */
+#define BC_UND_GREEN "\e[4;32m"         /* Green */
+#define BC_UND_YELLOW "\e[4;33m"        /* Yellow */
+#define BC_UND_BLUE "\e[4;34m"          /* Blue */
+#define BC_UND_PURPLE "\e[4;35m"        /* Purple */
+#define BC_UND_CYAN "\e[4;36m"          /* Cyan */
+#define BC_UND_WHITE "\e[4;37m"         /* White */
+
+#define BC_BAK_BLACK "\e[40m"         	/* Black - Background */
+#define BC_BAK_RED "\e[41m"           	/* Red */
+#define BC_BAK_GREEN "\e[42m"         	/* Green */
+#define BC_BAK_YELLOW "\e[43m"        	/* Yellow */
+#define BC_BAK_BLUE "\e[44m"          	/* Blue */
+#define BC_BAK_PURPLE "\e[45m"        	/* Purple */
+#define BC_BAK_CYAN "\e[46m"          	/* Cyan */
+#define BC_BAK_WHITE "\e[47m"         	/* White */
 
 #define BC_TXT_RST "\e[0m"		/* Text Reset */
 
--- test.c
+++ test.c
@@ -9,8 +9,13 @@ int main (int argc, char* argv[])
 	printf ("Tests for the DUtils library v. %s .\n\n", DUTILS_VERSION);
 
 	/* Bash Colors */
-	printf ("Bash Colors: %s Blue %s Yellow %s Reset!\n", BC_TXT_BLUE, BC_TXT_YELLOW, BC_TXT_RST);
+	printf ("Bash Colors TXT: %s Black %s Red %s Green %s Yellow %s Blue %s Purple %s Cyan %s White %s Reset!\n", BC_TXT_BLACK, BC_TXT_RED, BC_TXT_GREEN, BC_TXT_YELLOW, BC_TXT_BLUE, BC_TXT_PURPLE, BC_TXT_CYAN, BC_TXT_WHITE, BC_TXT_RST);
+	printf ("Bash Colors BLD: %s Black %s Red %s Green %s Yellow %s Blue %s Purple %s Cyan %s White %s Reset!\n", BC_BLD_BLACK, BC_BLD_RED, BC_BLD_GREEN, BC_BLD_YELLOW, BC_BLD_BLUE, BC_BLD_PURPLE, BC_BLD_CYAN, BC_BLD_WHITE, BC_TXT_RST);
+	printf ("Bash Colors UND: %s Black %s Red %s Green %s Yellow %s Blue %s Purple %s Cyan %s White %s Reset!\n", BC_UND_BLACK, BC_UND_RED, BC_UND_GREEN, BC_UND_YELLOW, BC_UND_BLUE, BC_UND_PURPLE, BC_UND_CYAN, BC_UND_WHITE, BC_TXT_RST);
+	printf ("Bash Colors BAK: %s Black %s Red %s Green %s Yellow %s Blue %s Purple %s Cyan %s White %s Reset!\n", BC_BAK_BLACK, BC_BAK_RED, BC_BAK_GREEN, BC_BAK_YELLOW, BC_BAK_BLUE, BC_BAK_PURPLE, BC_BAK_CYAN, BC_BAK_WHITE, BC_TXT_RST);
+
 	printf ("Test successful when the Words have their color!\n\n");
+	
 
 	/* Test End */
 	printf ("This is the end!\n");
