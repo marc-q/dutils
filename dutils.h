27a8634164f848ef0a6094fe8f5614398b4324b8
Marc Volker Dickmann <hq@devdiary.org>
2015-10-11 16:10:21 +0200

    Better compatibility.

--- dutils.h
+++ dutils.h
@@ -26,42 +26,42 @@
 
 /* BASH COLORS */
 
-#define BC_TXT_BLACK "\e[0;30m"		/* Black */
-#define BC_TXT_RED "\e[0;31m"		/* Red */
-#define BC_TXT_GREEN "\e[0;32m"		/* Green */
-#define BC_TXT_YELLOW "\e[0;33m"	/* Yellow */
-#define BC_TXT_BLUE "\e[0;34m"		/* Blue */
-#define BC_TXT_PURPLE "\e[0;35m"	/* Purple */
-#define BC_TXT_CYAN "\e[0;36m"		/* Cyan */
-#define BC_TXT_WHITE "\e[0;37m"		/* White */
+#define BC_TXT_BLACK "\033[0;30m"	/* Black */
+#define BC_TXT_RED "\033[0;31m"		/* Red */
+#define BC_TXT_GREEN "\033[0;32m"	/* Green */
+#define BC_TXT_YELLOW "\033[0;33m"	/* Yellow */
+#define BC_TXT_BLUE "\033[0;34m"	/* Blue */
+#define BC_TXT_PURPLE "\033[0;35m"	/* Purple */
+#define BC_TXT_CYAN "\033[0;36m"	/* Cyan */
+#define BC_TXT_WHITE "\033[0;37m"	/* White */
 
-#define BC_BLD_BLACK "\e[1;30m"		/* Black - Bold */
-#define BC_BLD_RED "\e[1;31m"		/* Red */
-#define BC_BLD_GREEN "\e[1;32m"		/* Green */
-#define BC_BLD_YELLOW "\e[1;33m"	/* Yellow */
-#define BC_BLD_BLUE "\e[1;34m"		/* Blue */
-#define BC_BLD_PURPLE "\e[1;35m"	/* Purple */
-#define BC_BLD_CYAN "\e[1;36m"		/* Cyan */
-#define BC_BLD_WHITE "\e[1;37m"		/* White */
+#define BC_BLD_BLACK "\033[1;30m"	/* Black - Bold */
+#define BC_BLD_RED "\033[1;31m"		/* Red */
+#define BC_BLD_GREEN "\033[1;32m"	/* Green */
+#define BC_BLD_YELLOW "\033[1;33m"	/* Yellow */
+#define BC_BLD_BLUE "\033[1;34m"	/* Blue */
+#define BC_BLD_PURPLE "\033[1;35m"	/* Purple */
+#define BC_BLD_CYAN "\033[1;36m"	/* Cyan */
+#define BC_BLD_WHITE "\033[1;37m"	/* White */
 
-#define BC_UND_BLACK "\e[4;30m"         /* Black - Underline*/
-#define BC_UND_RED "\e[4;31m"           /* Red */
-#define BC_UND_GREEN "\e[4;32m"         /* Green */
-#define BC_UND_YELLOW "\e[4;33m"        /* Yellow */
-#define BC_UND_BLUE "\e[4;34m"          /* Blue */
-#define BC_UND_PURPLE "\e[4;35m"        /* Purple */
-#define BC_UND_CYAN "\e[4;36m"          /* Cyan */
-#define BC_UND_WHITE "\e[4;37m"         /* White */
+#define BC_UND_BLACK "\033[4;30m"	/* Black - Underline*/
+#define BC_UND_RED "\033[4;31m"		/* Red */
+#define BC_UND_GREEN "\033[4;32m"	/* Green */
+#define BC_UND_YELLOW "\033[4;33m"	/* Yellow */
+#define BC_UND_BLUE "\033[4;34m"	/* Blue */
+#define BC_UND_PURPLE "\033[4;35m"	/* Purple */
+#define BC_UND_CYAN "\033[4;36m"	/* Cyan */
+#define BC_UND_WHITE "\033[4;37m"	/* White */
 
-#define BC_BAK_BLACK "\e[40m"         	/* Black - Background */
-#define BC_BAK_RED "\e[41m"           	/* Red */
-#define BC_BAK_GREEN "\e[42m"         	/* Green */
-#define BC_BAK_YELLOW "\e[43m"        	/* Yellow */
-#define BC_BAK_BLUE "\e[44m"          	/* Blue */
-#define BC_BAK_PURPLE "\e[45m"        	/* Purple */
-#define BC_BAK_CYAN "\e[46m"          	/* Cyan */
-#define BC_BAK_WHITE "\e[47m"         	/* White */
+#define BC_BAK_BLACK "\033[40m"		/* Black - Background */
+#define BC_BAK_RED "\033[41m"		/* Red */
+#define BC_BAK_GREEN "\033[42m"		/* Green */
+#define BC_BAK_YELLOW "\033[43m"	/* Yellow */
+#define BC_BAK_BLUE "\033[44m"		/* Blue */
+#define BC_BAK_PURPLE "\033[45m"	/* Purple */
+#define BC_BAK_CYAN "\033[46m"		/* Cyan */
+#define BC_BAK_WHITE "\033[47m"		/* White */
 
-#define BC_TXT_RST "\e[0m"		/* Text Reset */
+#define BC_TXT_RST "\033[0m"		/* Text Reset */
 
 #endif /* __DUTILS_H__ */
